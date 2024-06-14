b4495150e1a3cd95f5d8a393badd76e47ff5dfcd
HITOA <charli.poirier@gmail.com>
2024-06-14 20:54:15 +0200

    Correct typo

--- palette-generator/default.nix
+++ palette-generator/default.nix
@@ -1,6 +1,6 @@
 { buildNpmPackage, lib, ... }:
 buildNpmPackage rec {
-  npmDepsHash = lib.fakeHash;
+  npmDepsHash = "sha256-dVv/s9vPOjCLCsS5Q73PDKN4pvSSFgJCxrub2uiKaSA=";
   pname = "palette-generator";
   version = "1.0.0";
 
