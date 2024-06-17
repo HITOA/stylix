145f2247c857c9b195cb8fd27ff5245c6330adf8
HITOA <charli.poirier@gmail.com>
2024-06-17 19:24:43 +0200

    hopefully it's working now

--- palette-generator/default.nix
+++ palette-generator/default.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchgit {
     url = "https://github.com/HITOA/image2palette.git";
     rev = "0ed6de13ee07e4c6b14645892a1b25868f4ad82f";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-nQmPm/h/hXACEdfVyhIt67pyTgd4xwP2mT3hp7DuG14=";
   };
 
   buildPhase = ''
