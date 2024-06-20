6febf40957f50a2d94176469dd0971b429c4c29c
HITOA <charli.poirier@gmail.com>
2024-06-20 15:24:33 +0200

    test

--- palette-generator/default.nix
+++ palette-generator/default.nix
@@ -1,4 +1,11 @@
 { pkgs, lib, palette-generator-src }:
+let
+  palette-generator-src = {
+    inherit (inputs)
+      palette-generator-src
+      ;
+  };
+in
 pkgs.stdenv.mkDerivation {
   name = "palette-generator";
   src = palette-generator-src;
